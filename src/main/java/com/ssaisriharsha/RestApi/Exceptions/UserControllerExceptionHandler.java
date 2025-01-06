090b8e8c948f25b31d69cc6e395d70bad1d2520c
Harsha <69705177+ssaisriharsha@users.noreply.github.com>
2025-01-06 18:07:40 +0530

    Made the exceptions to obey HATEOAS format.

--- src/main/java/com/ssaisriharsha/RestApi/Exceptions/UserControllerExceptionHandler.java
+++ src/main/java/com/ssaisriharsha/RestApi/Exceptions/UserControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.ssaisriharsha.RestApi.Exceptions;
 
+import com.ssaisriharsha.RestApi.Controller.UserController;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,41 +9,63 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @ControllerAdvice
 public class UserControllerExceptionHandler {
     @ExceptionHandler
-    public ResponseEntity<UserNotFoundResponse> handleUserNotFound(UserNotFoundException e) {
+    public ResponseEntity<EntityModel<UserNotFoundResponse>> handleUserNotFound(UserNotFoundException e) {
         UserNotFoundResponse u = new UserNotFoundResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
-        return new ResponseEntity<>(u, HttpStatus.BAD_REQUEST);
+        EntityModel<UserNotFoundResponse> uModel = EntityModel.of(u,
+                linkTo(methodOn(UserController.class).getAllUsers()).withRel("users")
+                );
+        return new ResponseEntity<>(uModel, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<InvalidIdResponse> handleInvalidId() {
+    public ResponseEntity<EntityModel<InvalidIdResponse>> handleInvalidId() {
         InvalidIdResponse response = new InvalidIdResponse(HttpStatus.BAD_REQUEST.value(), System.currentTimeMillis());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        EntityModel<InvalidIdResponse> responseEntityModel = EntityModel.of(response,
+                linkTo(methodOn(UserController.class).getAllUsers()).withRel("users")
+                );
+        return new ResponseEntity<>(responseEntityModel, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<ResourceNotFoundResponse> handleResourceNotFound() {
+    public ResponseEntity<EntityModel<ResourceNotFoundResponse>> handleResourceNotFound() {
         ResourceNotFoundResponse response = new ResourceNotFoundResponse(System.currentTimeMillis());
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        EntityModel<ResourceNotFoundResponse> responseEntityModel = EntityModel.of(response,
+        linkTo(methodOn(UserController.class).getAllUsers()).withRel("users")
+        );
+        return new ResponseEntity<>(responseEntityModel, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler
-    public ResponseEntity<UserNotFoundResponse> handleAllExceptions(Exception e) {
+    public ResponseEntity<EntityModel<UserNotFoundResponse>> handleAllExceptions(Exception e) {
         UserNotFoundResponse u = new UserNotFoundResponse(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
-        return new ResponseEntity<>(u, HttpStatus.NOT_FOUND);
+        EntityModel<UserNotFoundResponse> uModel = EntityModel.of(u,
+                linkTo(methodOn(UserController.class).getAllUsers()).withRel("users")
+                );
+        return new ResponseEntity<>(uModel, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler
-    public ResponseEntity<UserExistsResponse> handleUserExistsException(UserExistsException e) {
+    public ResponseEntity<EntityModel<UserExistsResponse>> handleUserExistsException(UserExistsException e) {
         UserExistsResponse u = new UserExistsResponse(HttpStatus.CONFLICT.value(), e.getMessage(), System.currentTimeMillis());
-        return new ResponseEntity<>(u, HttpStatus.CONFLICT);
+        EntityModel<UserExistsResponse> uModel = EntityModel.of(u,
+                linkTo(methodOn(UserController.class).getAllUsers()).withRel("users")
+                );
+        return new ResponseEntity<>(uModel, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler
-    public ResponseEntity<IllegalOperationResponse> handleIllegalOperationException(IllegalOperationException e) {
+    public ResponseEntity<EntityModel<IllegalOperationResponse>> handleIllegalOperationException(IllegalOperationException e) {
         IllegalOperationResponse res = new IllegalOperationResponse(e.getStatus(), e.getMessage(), System.currentTimeMillis());
-        return new ResponseEntity<>(res, HttpStatus.valueOf(e.getStatus()));
+        EntityModel<IllegalOperationResponse> resModel = EntityModel.of(res,
+                linkTo(methodOn(UserController.class).getAllUsers()).withRel("users")
+                );
+        return new ResponseEntity<>(resModel, HttpStatus.valueOf(e.getStatus()));
     }
 }
